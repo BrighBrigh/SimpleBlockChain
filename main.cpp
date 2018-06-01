cca6a5abdc2c037184bc87b0c04189aa33d174a5
brigh <brianewinkler@gmail.com>
2018-06-01 14:34:40 -0700

    Added block time

--- main.cpp
+++ main.cpp
@@ -9,23 +9,26 @@ int main(){
 
 	cout << "Starting...\n";
 
-
-	unsigned difficulty = 6;
+	unsigned difficulty = 5;
 	vector<Block> blockChain;
-
 
 	Block *tempBlock = new Block("Genesis", "0");
 	blockChain.push_back(*tempBlock);
-	cout << "Attempting to mine block...\n";
+	cout << "Attempting to mine Genesis block...\n";
 	blockChain.at(0).mineBlock(difficulty);
 
 	unsigned count = 0;
 
 	while(1){
 
+		if(count > 1){
+			unsigned blockTime = blockChain.at(count).timeStamp - blockChain.at(count-1).timeStamp;
+			cout << "\n\tTime to find:  " << blockTime << " seconds"; 
+		} 
+
 		Block *nextBlock = new Block("Block " + to_string(count), blockChain.at(count).hash);
 		blockChain.push_back(*nextBlock);
-		cout << "\n\nAttempting to mine block...\n";
+		cout << "\n\nAttempting to mine block...\n\n";
 
 		blockChain.at(count+1).mineBlock(difficulty);
 
